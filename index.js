12d5630e40d2a9fb538174fe409aa5e9582b15f4
AzizTafougalti <aziz.walker19@gmail.com>
2018-09-17 19:58:55 +0100

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const app = require('express')()
 const HTTPGraphQL = require('express-graphql')
 const schema = require('./Types')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.use('/graphql', HTTPGraphQL({
+app.use(cors())
+app.use('/graphql', bodyParser.json(), HTTPGraphQL({
     schema,
     graphiql: true
 }))
